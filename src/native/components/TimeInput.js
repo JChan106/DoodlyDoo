d79c096a6063a6cfc40787be05999141b4a61472
Abe Hu <abe@ucrwpa4-4-111-153.wnet.ucr.edu>
2018-03-12 10:50:20 -0700

    asdf

--- src/native/components/TimeInput.js
+++ src/native/components/TimeInput.js
@@ -109,10 +109,12 @@ class TimeInput extends React.Component {
         let userName = `${this.props.member.firstName} ${this.props.member.lastName}`;
         let recipeInfo = this.props.recipes.recipe;
         FirebaseRef.child('appointments').child(recipeInfo.masteruid).child(recipeInfo.id).child('invitedUsers').child(userName).update({inputted: false});
+        FirebaseRef.child('invitedAppointments').child(email).child(recipeInfo.id).child('invitedUsers').child(userName).update({canAttend: false});
       } else {
         let userName = `${this.props.member.firstName} ${this.props.member.lastName}`;
         let recipeInfo = this.props.recipes.recipe;
         FirebaseRef.child('appointments').child(recipeInfo.masteruid).child(recipeInfo.id).child('invitedUsers').child(userName).update({inputted: true});
+        FirebaseRef.child('invitedAppointments').child(email).child(recipeInfo.recipe.id).child('invitedUsers').child(userName).update({canAttend: true});
       }
     this.props.getRecipes(this.props.member.uid);
 
